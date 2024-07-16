6ff204137f30d8e68dd4065db3d3f06371326fe4
Jan Lehigh <janlehigh461437@gmail.com>
2024-07-16 17:01:49 -0600

    Add us 3 query

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,5 +2,6 @@ class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
     @recipe_ingredients = RecipeIngredient.find_recipe_ingredients(params[:id])
+    @recipe_ingredients_cost = RecipeIngredient.find_recipe_ingredients_cost(params[:id])
   end
 end
--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -6,4 +6,8 @@ class RecipeIngredient < ApplicationRecord
   def self.find_recipe_ingredients(recipe)
     RecipeIngredient.joins(:ingredient).where(recipe_id: recipe).select("ingredients.*")
   end
+
+  def self.find_recipe_ingredients_cost(recipe)
+    RecipeIngredient.joins(:ingredient).where(recipe_id: recipe).select("ingredients.*").sum(:cost)
+  end
 end
