906f2c53b331ee0ac972658dcd466f2b34ca0225
Jan Lehigh <janlehigh461437@gmail.com>
2024-07-16 16:57:55 -0600

    finnished us 2

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,6 +2,5 @@ class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
     @recipe_ingredients = RecipeIngredient.find_recipe_ingredients(params[:id])
-    binding.pry
   end
 end
